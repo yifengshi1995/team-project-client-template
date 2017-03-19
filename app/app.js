11b016aee80bb044210f8c8b209e3368ff4bca17
Yifeng Shi <byoukin@gmail.com>
2017-03-19 17:32:42 -0400

    Change to React style. Only modified main part

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Grid from './components/grid.js';
+
+if (document.getElementById('grid-main') !== null){
+  ReactDOM.render(
+    <Grid />,
+    document.getElementById('grid-main')
+  );
+}
---
+++ app/components/grid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gridmain from './gridmain.js';
+
+export default class Grid extends React.Component {
+  render(){
+    return(
+      <div>
+        <Gridmain />
+      </div>
+    )
+  }
+}
