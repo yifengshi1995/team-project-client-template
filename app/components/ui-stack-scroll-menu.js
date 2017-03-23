c26e9d3e6c209bffc447a891ee8592e803534c25
Craig Norton <craigbnorton@gmail.com>
2017-03-23 17:02:10 -0400

    This is the new edit card in react form

---
+++ app/components/ui-stack-scroll-menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class STACKPREIVEWSLIDER extends React.Component {
+  render() {
+    return (
+      <div>
+      <div className="scrollmenu">
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+          <a href="#">7</a>
+          <a href="#">8</a>
+          <a href="#">9</a>
+          <a href="#">10</a>
+          <a href="#">11</a>
+          <a href="#">12</a>
+      </div>
+      </div>
+    )
+  }
+}
