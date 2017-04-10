29e52c1dc24f6ac880ef174908986562507e63d9
Yifeng Shi <byoukin@gmail.com>
2017-04-09 22:19:15 -0400

    Finished basic routing from home to grid

--- app/components/stackfeeditem.js
+++ app/components/stackfeeditem.js
@@ -1,44 +1,37 @@
 import React from 'react';
 import {getStackData} from '../server';
-import {unixTimeToString} from '../util.js'
+import {unixTimeToString} from '../util';
+import {Link} from 'react-router';
 
 export default class Stackfeeditem extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = this.props.data;
+        this.state = {
+          data: this.props.data,
+          user: this.props.userid
+        };
     }
 
-    /*componentDidMount() {
-        getStackData(this.props.data, (stackData) => {
-            this.setState(stackData);
-        })
-    }*/
     render() {
-        var data = this.state;
-        console.log(data);
+        var data = this.state.data;
         return (
             <div>
                 <div className="search-result">
                     <div className="search-deck-preview">
-
                         <div className="stack-thumbnail">
                             <img src={'../build/img/default-thumbnail.jpg'}/>
                         </div>
-
                     </div>
-
                     <div className="search-deck-info">
                         <div className="stack-name">
-                            <a href="#">{data.name}</a>
+                            <Link to={"/grid/" + this.state.data._id}>{data.name}</Link>
                         </div>
                         <div className="stack-info">
                             {data.cards.length} cards  ·  Posted {unixTimeToString(data.postDate)}
                         </div>
-
                     </div>
                 </div>
-
             </div>
         )
     }
