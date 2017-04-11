d64e093cd10f9a1fbbb3cd0859ea4b9eb6c549d0
Liam Ernest <lernest@umass.edu>
2017-04-11 16:45:15 -0400

    added random quote box to homepage

--- app/components/home.js
+++ app/components/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {currentTimeToString}  from '../util';
+import {randomQuote} from '../util';
 import {getUserData} from '../server';
 import UINAVBAR from './ui-nav-bar.js';
 import UISIDEBAR from './ui-side-bar.js';
@@ -31,9 +32,26 @@ export default class Home extends React.Component {
             </div>
 
             <div className="col-md-8 main-home-section">
-                Your Stacks
-                <br />
+            <div className="row">
+            <div className ="col-md-12 welcome">
+              <h1> Hello {this.state.fullName} </h1>
+              <h2> Welcome to SmartCards! </h2>
+                    <div className = "col-md-2"></div>
+                    <div className="col-md-8 quote">
+                        <h3>INSPIRATIONAL QUOTE OF THE DAY</h3>
+                        <p>{randomQuote()}</p>
+                    </div>
+                    <div className = "col-md-2"></div>
+                    </div>
+
+
+            </div>
+
+
+                <h4>Your Stacks</h4>
+                
                 <Stackfeed user={this.state.user} />
+
             </div>
                 <div className="col-md-3 right-sidebar">
                     <div className="row profile">
